465889a1eb7f84a19f262125716b460f83f242fa
harshad.chavan <harshad.chavan@exponentia.ai>
2022-03-02 23:48:23 +0530

    Code base done

--- base/urls.py
+++ base/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('create_room/',views.createRoom,name="create_room"),
     path('update_room/<str:pk>',views.updateRoom,name="update_room"),
     path('delete_room/<str:pk>',views.deleteRoom,name="delete_room"),
-    path('delete_message/<str:pk>',views.deleteMessage,name="delete_message")
+    path('delete_message/<str:pk>',views.deleteMessage,name="delete_message"),
+    path('profile/<str:pk>/',views.userProfile,name="user_profile"),
     
 
 ]
