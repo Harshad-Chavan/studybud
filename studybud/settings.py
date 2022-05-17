c9f283e29bd44ebd98752394528b6d7ba44eccbc
harshad.chavan <harshad.chavan@exponentia.ai>
2022-05-17 12:31:42 +0530

    secret_key from env

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o$wl^(ktmxz(j8srrpda&!yq$i7bq@7yxk=gbddrqf3_3rs06d'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
